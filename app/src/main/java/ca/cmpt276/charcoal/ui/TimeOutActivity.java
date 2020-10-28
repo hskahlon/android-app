092c5e149f727c2a937d28529c5e292e0104bc21
phyeony <phyeony@gmail.com>
2020-10-28 16:19:14 -0700

    [Tomas] Add preset time spinner

--- app/src/main/java/ca/cmpt276/charcoal/ui/TimeOutActivity.java
+++ app/src/main/java/ca/cmpt276/charcoal/ui/TimeOutActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,18 +18,19 @@ import java.util.Locale;
 
 import ca.cmpt276.charcoal.practicalparent.R;
 
-public class TimeOutActivity extends AppCompatActivity {
+public class TimeOutActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private long startTimeInMillis;
 
     private TextView countDownText;
-    private Button startButton, pauseButton, cancelButton,setButton;
+    private Button startButton, pauseButton, cancelButton, setButton;
 
     private EditText setTimeText;
-
 
     private boolean isTimerRunning;
     private long timeLeftInMillis = startTimeInMillis;
     private CountDownTimer countDownTimer;
+
+    private Spinner preSetTimeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,30 +48,57 @@ public class TimeOutActivity extends AppCompatActivity {
 
         setTimeText = (EditText) findViewById(R.id.setTimeText);
 
+        preSetTimeSpinner = (Spinner) findViewById(R.id.preSetTimeSpinner);
+
         setupSetButton();
         setupStartButton();
         setupCancelButton();
         setupPauseButton();
 
+        setupSpinner();
     }
 
+    private void setupSpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.preSetTimes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        preSetTimeSpinner.setAdapter(adapter);
+        preSetTimeSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String preSetTime = parent.getItemAtPosition(position).toString();
+        long millisInput = Long.parseLong(preSetTime) * 60000;
+        setTime(millisInput);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
     private void updateUI() {
-        if(isTimerRunning){
+        if (isTimerRunning) {
             cancelButton.setVisibility(View.VISIBLE);
             pauseButton.setVisibility(View.VISIBLE);
             pauseButton.setText("Pause");
             startButton.setVisibility(View.INVISIBLE);
 
+            preSetTimeSpinner.setVisibility(View.INVISIBLE);
+
             setButton.setVisibility(View.INVISIBLE);
             setTimeText.setVisibility(View.INVISIBLE);
-        }
-        else{
+        } else {
             //If the timer is done
             if (timeLeftInMillis < 1000) {
                 cancelButton.setVisibility(View.INVISIBLE);
                 pauseButton.setVisibility(View.INVISIBLE);
                 pauseButton.setText("Pause");
                 startButton.setVisibility(View.VISIBLE);
+
+                preSetTimeSpinner.setVisibility(View.VISIBLE);
 
                 setButton.setVisibility(View.VISIBLE);
                 setTimeText.setVisibility(View.VISIBLE);
@@ -82,11 +113,12 @@ public class TimeOutActivity extends AppCompatActivity {
         }
 
     }
+
     private void setupPauseButton() {
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isTimerRunning){
+                if (isTimerRunning) {
                     pauseTimer();
                 } else {
                     startTimer();
@@ -108,8 +140,8 @@ public class TimeOutActivity extends AppCompatActivity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(timeLeftInMillis < 1000){
-                    Toast.makeText(TimeOutActivity.this,"No Timer Made",Toast.LENGTH_SHORT).show();
+                if (timeLeftInMillis < 1000) {
+                    Toast.makeText(TimeOutActivity.this, "No Timer Made", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 startTimer();
@@ -159,7 +191,7 @@ public class TimeOutActivity extends AppCompatActivity {
         if (countDownTimer != null) {
             countDownTimer.cancel();
         }
-        timeLeftInMillis= startTimeInMillis;
+        timeLeftInMillis = startTimeInMillis;
         updateCountDownText();
 
         cancelButton.setVisibility(View.INVISIBLE);
@@ -167,6 +199,7 @@ public class TimeOutActivity extends AppCompatActivity {
         pauseButton.setVisibility(View.INVISIBLE);
         setButton.setVisibility(View.VISIBLE);
         setTimeText.setVisibility(View.VISIBLE);
+        preSetTimeSpinner.setVisibility(View.VISIBLE);
 //      updateUI();
     }
 
@@ -211,6 +244,4 @@ public class TimeOutActivity extends AppCompatActivity {
         updateUI();
 
     }
-
-
 }
