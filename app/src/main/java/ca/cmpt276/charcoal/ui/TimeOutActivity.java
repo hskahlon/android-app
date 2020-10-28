fe3fa80cf44cd2c603bca7feab5660b7a7a38874
phyeony <phyeony@gmail.com>
2020-10-28 14:58:45 -0700

    [Tomas] Make time input for itmer

--- app/src/main/java/ca/cmpt276/charcoal/ui/TimeOutActivity.java
+++ app/src/main/java/ca/cmpt276/charcoal/ui/TimeOutActivity.java
@@ -5,21 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 import ca.cmpt276.charcoal.practicalparent.R;
 
 public class TimeOutActivity extends AppCompatActivity {
-    private static final long START_TIME_IN_MILLIS = 6000;
+    private long startTimeInMillis;
 
     private TextView countDownText;
-    private Button startButton, pauseButton, cancelButton;
+    private Button startButton, pauseButton, cancelButton,setButton;
+
+    private EditText setTimeText;
+
 
     private boolean isTimerRunning;
-    private long timeLeftInMillis = START_TIME_IN_MILLIS;
+    private long timeLeftInMillis = startTimeInMillis;
     private CountDownTimer countDownTimer;
 
     @Override
@@ -30,30 +36,53 @@ public class TimeOutActivity extends AppCompatActivity {
         //Reference: https://codinginflow.com/tutorials/android/countdowntimer/part-1-countdown-timer
 
         countDownText = (TextView) findViewById(R.id.countDownText);
+
         startButton = (Button) findViewById(R.id.startBtn);
         pauseButton = (Button) findViewById(R.id.pauseBtn);
         cancelButton = (Button) findViewById(R.id.cancelBtn);
+        setButton = (Button) findViewById(R.id.setBtn);
 
+        setTimeText = (EditText) findViewById(R.id.setTimeText);
 
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                if(isTimerRunning){
-//                    pauseTimer();
-//                } else {
-//                    startTimer();
-//                }
-                startTimer();
+        setupSetButton();
+        setupStartButton();
+        setupCancelButton();
+        setupPauseButton();
+
+    }
+
+    private void updateUI() {
+        if(isTimerRunning){
+            cancelButton.setVisibility(View.VISIBLE);
+            pauseButton.setVisibility(View.VISIBLE);
+            pauseButton.setText("Pause");
+            startButton.setVisibility(View.INVISIBLE);
+
+            setButton.setVisibility(View.INVISIBLE);
+            setTimeText.setVisibility(View.INVISIBLE);
+        }
+        else{
+            //If the timer is done
+            if (timeLeftInMillis < 1000) {
+                cancelButton.setVisibility(View.INVISIBLE);
+                pauseButton.setVisibility(View.INVISIBLE);
+                pauseButton.setText("Pause");
+                startButton.setVisibility(View.VISIBLE);
+
+                setButton.setVisibility(View.VISIBLE);
+                setTimeText.setVisibility(View.VISIBLE);
             }
-        });
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cancelTimer();
+            //if the timer is paused
+            else if (timeLeftInMillis < startTimeInMillis) {
+                cancelButton.setVisibility(View.VISIBLE);
+                pauseButton.setVisibility(View.VISIBLE);
+                pauseButton.setText("Resume");
+                startButton.setVisibility(View.INVISIBLE);
             }
-        });
+        }
 
+    }
+    private void setupPauseButton() {
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,21 +95,90 @@ public class TimeOutActivity extends AppCompatActivity {
         });
     }
 
+    private void setupCancelButton() {
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelTimer();
+            }
+        });
+    }
+
+    private void setupStartButton() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
+        });
+    }
+
+    private void setupSetButton() {
+        setButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = setTimeText.getText().toString();
+
+                //If input is empty
+                if (input.length() == 0) {
+                    Toast.makeText(TimeOutActivity.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                long millisInput = Long.parseLong(input) * 60000;
+                //If entered 0
+                if (millisInput == 0) {
+                    Toast.makeText(TimeOutActivity.this, "Please enter a positive number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                setTime(millisInput);
+                setTimeText.setText("");
+            }
+        });
+    }
+
+    private void setTime(long milliseconds) {
+        startTimeInMillis = milliseconds;
+        cancelTimer();
+        closeKeyboard();
+    }
+
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    //TODO: Refactor updating buttons to updateUI()
     private void cancelTimer() {
-        countDownTimer.cancel();
-        timeLeftInMillis= START_TIME_IN_MILLIS;
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        timeLeftInMillis= startTimeInMillis;
         updateCountDownText();
+
         cancelButton.setVisibility(View.INVISIBLE);
         startButton.setVisibility(View.VISIBLE);
         pauseButton.setVisibility(View.INVISIBLE);
-        startButton.setText("Start");
-
+        setButton.setVisibility(View.VISIBLE);
+        setTimeText.setVisibility(View.VISIBLE);
+//      updateUI();
     }
 
     private void updateCountDownText() {
-        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int hours = (int) (timeLeftInMillis / 1000) / 3600;
+        int minutes = (int) ((timeLeftInMillis / 1000) % 3600) / 60;
         int seconds = (int) (timeLeftInMillis / 1000) % 60;
-        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        String timeLeftFormatted;
+        if (hours > 0) {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%d:%02d:%02d", hours, minutes, seconds);
+        } else {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%02d:%02d", minutes, seconds);
+        }
         countDownText.setText(timeLeftFormatted);
     }
 
@@ -95,26 +193,19 @@ public class TimeOutActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 isTimerRunning = false;
-                startButton.setText("Start");
-                startButton.setVisibility(View.VISIBLE);
-                cancelButton.setVisibility(View.INVISIBLE);
-                pauseButton.setVisibility(View.INVISIBLE);
+                updateUI();
             }
         }.start();
-        startButton.setVisibility(View.INVISIBLE);
+
         isTimerRunning = true;
-        startButton.setText("Pause");
-        cancelButton.setVisibility(View.VISIBLE);
-        pauseButton.setVisibility(View.VISIBLE);
-        pauseButton.setText("Pause");
+        updateUI();
     }
 
     private void pauseTimer() {
         countDownTimer.cancel();
         isTimerRunning = false;
-        startButton.setText("Start");
-        cancelButton.setVisibility(View.VISIBLE);
-        pauseButton.setText("Start");
+        updateUI();
+
     }
 
 
