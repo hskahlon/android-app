ddb5423612573baf0a8f5724e9cd6b9bfc4a80c5
Bryan Niwa <thebryanniwa@gmail.com>
2020-10-28 22:51:23 -0700

    Add ChildrenActivity to display list of known children.
    List is currently just a placeholder list. Clicking FAB displays a toast

--- app/src/main/java/ca/cmpt276/charcoal/practicalparent/MainActivity.java
+++ app/src/main/java/ca/cmpt276/charcoal/practicalparent/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import ca.cmpt276.charcoal.practicalparent.ui.ChildrenActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(ChildrenActivity.makeLaunchIntent(this));
     }
 }
---
+++ app/src/main/java/ca/cmpt276/charcoal/practicalparent/ui/ChildrenActivity.java
@@ -0,0 +1,69 @@
+package ca.cmpt276.charcoal.practicalparent.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import ca.cmpt276.charcoal.practicalparent.R;
+
+public class ChildrenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_children);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.addChild_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ChildrenActivity.this, "Take me to AddChildActivity", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        populateListView();
+        registerClickCallback();
+    }
+
+    public static Intent makeLaunchIntent(Context context) {
+        return new Intent(context, ChildrenActivity.class);
+    }
+
+    private void populateListView() {
+        String[] children = {"Jimmy", "Beth", "Walter"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.child_item, children);
+
+        ListView list = findViewById(R.id.children_list);
+        list.setAdapter(adapter);
+    }
+
+    private void registerClickCallback() {
+        ListView list = findViewById(R.id.children_list);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+                TextView textView = (TextView) viewClicked;
+                String message = "You clicked " + textView.getText();
+                Toast.makeText(ChildrenActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
