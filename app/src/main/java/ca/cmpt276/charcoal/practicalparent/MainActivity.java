0e86d931a8c7e1aa37ee50182c35cc5361b4da7b
Jason Combs <jcombs@sfu.ca>
2020-11-01 22:25:08 -0800

    Main menu button selection skeleton

--- app/src/main/java/ca/cmpt276/charcoal/practicalparent/MainActivity.java
+++ app/src/main/java/ca/cmpt276/charcoal/practicalparent/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
             Intent i = CoinFlipActivity.makeLaunchIntent(MainActivity.this);
             startActivity(i);
         });
-
     }
 
 }
