678f46c3195ed0f20d048ae72cbd1ec9cd4f7c19
bniwa <bryan_niwa@sfu.ca>
2020-11-04 12:22:20 -0800

    Create basic notification that is shown when timer finishes.
    Currently, there are no actions associated with the notification.

--- app/src/main/java/ca/cmpt276/charcoal/practicalparent/TimeOutActivity.java
+++ app/src/main/java/ca/cmpt276/charcoal/practicalparent/TimeOutActivity.java
@@ -3,8 +3,11 @@ package ca.cmpt276.charcoal.practicalparent;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 
+import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -172,6 +175,15 @@ public class TimeOutActivity extends AppCompatActivity implements AdapterView.On
             r.stop();
             v.cancel();
         }, 5000);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.timout_alarm_notification_ID))
+                .setSmallIcon(R.drawable.ic_baseline_alarm_24)
+                .setContentTitle(getString(R.string.timeout_notification_title))
+                .setContentText(getString(R.string.timeout_notification_body))
+                .setPriority(NotificationCompat.PRIORITY_MAX);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(123456, builder.build());
     }
 
     private void setupPauseButton() {
