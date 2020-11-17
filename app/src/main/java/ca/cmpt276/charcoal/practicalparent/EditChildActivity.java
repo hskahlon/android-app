2f943820483a7d367bc55405f74ac7906c837707
phyeony <phyeony@gmail.com>
2020-11-17 12:19:15 -0800

    Organize files

--- app/src/main/java/ca/cmpt276/charcoal/practicalparent/EditChildActivity.java
+++ app/src/main/java/ca/cmpt276/charcoal/practicalparent/EditChildActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
