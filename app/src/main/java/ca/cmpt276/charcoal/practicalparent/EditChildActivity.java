698c3483aa59a4659c937f9aa5f788fa6378c12e
Jason Combs <jcombs@sfu.ca>
2020-11-21 08:12:12 -0800

    Finally got the edit child screen to display the gallery.
    Next steps: setting up image IDs, testing image selection

--- app/src/main/java/ca/cmpt276/charcoal/practicalparent/EditChildActivity.java
+++ app/src/main/java/ca/cmpt276/charcoal/practicalparent/EditChildActivity.java
@@ -3,11 +3,12 @@ package ca.cmpt276.charcoal.practicalparent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -44,6 +45,8 @@ public class EditChildActivity extends AppCompatActivity {
     private EditText nameBox;
     private final ChildManager manager = ChildManager.getInstance();
     private final TasksManager tasksManager = TasksManager.getInstance();
+    ImageView imageToUpload;
+    private static final int RESULT_LOAD_IMAGE = 1;
 
     public static Intent makeLaunchIntent(Context context, int childIndex) {
         Intent intent = new Intent(context, EditChildActivity.class);
@@ -74,8 +77,8 @@ public class EditChildActivity extends AppCompatActivity {
 
         extractIntentData();
         preFillNameBox();
-        setupEditImageUploadButton();
-        setupEditImageCameraButton();
+        setupImportImageButton();
+//        setupEditImageCameraButton();
     }
 
     private void preFillNameBox() {
@@ -86,32 +89,38 @@ public class EditChildActivity extends AppCompatActivity {
     }
 
     // TODO: Finish this function and its Activity
-    private void setupEditImageUploadButton() {
-        Button editImageUploadButton = findViewById(R.id.editImageUpload_btn);
-        editImageUploadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(EditChildActivity.this, "Upload a photo", Toast.LENGTH_SHORT)
-                        .show();
-                Intent intent = new Intent(EditChildActivity.this, EditChildImageUploadActivity.class);
-                startActivity(intent);
-            }
-        });
+    private void setupImportImageButton() {
+        Button importImageButton = findViewById(R.id.editImageUpload_btn);
+        importImageButton.setOnClickListener(v -> dispatchChooseImportIntent());
+    }
+
+    private void dispatchChooseImportIntent() {
+        Intent choosePictureIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(choosePictureIntent, RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
     }
 
     // TODO: Finish this function and its Activity
-    private void setupEditImageCameraButton() {
-        Button editImageCameraButton = findViewById(R.id.editImageCamera_btn);
-        editImageCameraButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(EditChildActivity.this, "Take a photo", Toast.LENGTH_SHORT)
-                        .show();
-                Intent intent = new Intent(EditChildActivity.this, EditChildImageCameraActivity.class);
-//                startActivity(intent);
-            }
-        });
-    }
+//    private void setupEditImageCameraButton() {
+//        Button editImageCameraButton = findViewById(R.id.editImageCamera_btn);
+//        editImageCameraButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(EditChildActivity.this, "Take a photo", Toast.LENGTH_SHORT)
+//                        .show();
+//                Intent intent = new Intent(EditChildActivity.this, EditChildImageCameraActivity.class);
+////                startActivity(intent);
+//            }
+//        });
+//    }
 
     private void setupSaveButton() {
         Button saveBtn = findViewById(R.id.editChildSave_btn);
